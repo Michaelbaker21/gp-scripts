f8cba34e3d58c2e335087810a2e149a9f8799333
Michaelbaker21 <80808485+Michaelbaker21@users.noreply.github.com>
2026-04-08 12:27:21 +1000

    Update slider.js

--- slider.js
+++ slider.js
@@ -7,8 +7,12 @@
     clearInterval(i);
     b.style.setProperty('max-width','none','important');
     b.style.setProperty('width',w.offsetWidth+'px','important');
+    d.style.setProperty('left','0','important');
+    d.style.setProperty('margin-left',(w.offsetWidth/2)+'px','important');
     window.addEventListener('resize',function(){
-      b.style.setProperty('width',w.offsetWidth+'px','important');
+      var pw=w.offsetWidth;
+      b.style.setProperty('width',pw+'px','important');
+      d.style.setProperty('margin-left',(pw/2)+'px','important');
     });
     var a=false;
     function s(cx){
@@ -17,7 +21,7 @@
       var right=(100-p).toFixed(1);
       b.style.setProperty('clip-path','inset(0 '+right+'% 0 0)','important');
       b.style.setProperty('-webkit-clip-path','inset(0 '+right+'% 0 0)','important');
-      d.style.setProperty('left',p.toFixed(1)+'%','important');
+      d.style.setProperty('margin-left',(r.width*p/100).toFixed(1)+'px','important');
     }
     w.style.cursor='grab';
     w.addEventListener('pointerdown',function(e){
@@ -36,13 +40,14 @@
         var t=Math.min(1,(Date.now()-start)/1100);
         var ease=t<0.3?t/0.3*-3:t<0.6?(t-0.3)/0.3*6-3:(t-0.6)/0.4*-3+3;
         var p=50+ease;
+        var pw=w.offsetWidth;
         b.style.setProperty('clip-path','inset(0 '+(100-p).toFixed(1)+'% 0 0)','important');
         b.style.setProperty('-webkit-clip-path','inset(0 '+(100-p).toFixed(1)+'% 0 0)','important');
-        d.style.setProperty('left',p.toFixed(1)+'%','important');
+        d.style.setProperty('margin-left',(pw*p/100).toFixed(1)+'px','important');
         if(t<1)requestAnimationFrame(tick);
         else{
           b.style.setProperty('clip-path','inset(0 50% 0 0)','important');
-          d.style.setProperty('left','50%','important');
+          d.style.setProperty('margin-left',(pw/2)+'px','important');
         }
       }
       requestAnimationFrame(tick);
